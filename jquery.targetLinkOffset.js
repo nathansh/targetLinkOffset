100874c120a7d1d2dc2c99a655f79796191c0b11
Nathan Shubert-Harbison <nathan@domain7.com>
2014-10-23 16:44:08 -0700

    initial commit

---
+++ jquery.targetLinkOffset.js
@@ -0,0 +1,92 @@
+(function ($) {
+
+	var tlo = {
+
+		defaults: {
+
+			settings: {},
+			offset: 100,
+			buffer: 25,
+			linkSelector: "a[href^='#']",
+			boundEvent: "click.targetLinkOffset",
+			animate: false,
+			animationSpeed: 800
+
+		}, // defaults
+
+		goToHash: function(hash, dontPushState) {
+
+			// Only do something if the hash isn't just '#'
+			if ( hash !== '#' ) {
+
+			 	// Get the scrollTop to the hash
+			 	var element = $(hash),
+			 			scrollTarget = Math.floor(element.offset().top - (tlo.settings.offset + tlo.settings.buffer));
+
+			 	// Replace the url, method depending on whether pushState is supported or not
+			 	if ( !dontPushState ) {
+				 	if ( history.pushState ) {
+				 		history.pushState(null, null, hash);
+				 	} else {
+				 		location.hash = hash;
+				 	}					 		
+			 	}
+
+			 	// Scroll to element, either with an animation or directly
+			 	if ( tlo.settings.animate ) {
+			 		$('html, body').animate({
+			 			scrollTop: scrollTarget
+			 		}, tlo.settings.animationSpeed);
+			 	} else {
+					$(document, 'body').scrollTop(scrollTarget);
+			 	}
+
+			} // hash == #
+
+		}, // goToHash()
+
+		init: function() {
+
+			// Bind efent, default is click
+			$(tlo.settings.linkSelector).on(tlo.settings.boundEvent, function(event){
+
+				// Get the target id
+				var href = $(this).attr('href');
+
+				// Go to the hash
+				if ( href !== '#' ) {
+					tlo.goToHash(href);
+				 	event.preventDefault();			
+				}
+			
+			}); // on.boundEvent
+
+			// On load see if there's a hash in the url and go to it
+			if ( document.location.hash ) {
+				tlo.goToHash(document.location.hash, true);
+			}
+
+			// Also check hashchange
+			$(window).bind('hashchange', function() {
+				var hash = "#"+window.location.hash.replace(/^#/,'');
+				tlo.goToHash(hash, true);
+			}); // hashchange
+
+		} // init
+
+	}; // tlo
+
+
+	$.extend({
+		targetLinkOffset: function(options) {
+
+			// Merge settings
+			tlo.settings = $.extend(tlo.defaults, options);
+
+			// Run the main init
+			tlo.init();
+
+		}
+	});
+
+})(jQuery);
---
+++ readme.md
@@ -0,0 +1,27 @@
+# targetLinkOffset
+
+This plugin addresses an issue that arises when `position: fixed;` headers are combined with target links (e.g. `<a href="#section2">`). We all know what happens next. If your header is 100px tall, the content you've navigated to will have the top 100px hidden by the header. This plugin deals with that issue.
+
+## Usage
+
+Include jQuery and instantiate the plugin
+
+	<script src="path/to/jquery.min.js"></script>
+	<script src="path/to/jquery.targetLinkOffset.min.js"></script>
+	<script>
+	    jQuery(document).ready(function($) {
+	        $.targetLinkOffset({
+	            offset: $('#masthead').outerHeight(),
+	            buffer: 22
+	        });
+	    });
+	</script>
+
+## Options
+
+	offset: 100, // This option is absolutely necessary. This should be the calculated height of your header
+	buffer: 25, // This is some extra space under your header so the content isn't flush with the header's bottom
+	linkSelector: "a[href^='#']", // Specify a different selector for target links
+	boundEvent: "click.targetLinkOffset", // If you don't want bind this to click events
+	animate: false, // Animation, disabled by default
+	animationSpeed: 800 // You guessed it.
